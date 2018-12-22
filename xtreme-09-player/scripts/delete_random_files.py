428666b0f19bb6ff2f85ef85cc9d5fbacfdb15df
Preston Werntz <werntzp@hotmail.com>
2018-12-21 21:08:26 -0500

    Update delete_random_files.py

--- xtreme-09-player/scripts/delete_random_files.py
+++ xtreme-09-player/scripts/delete_random_files.py
@@ -5,10 +5,11 @@
 # ==========================
 import os
 import random
+import sys
 
 for f in os.listdir(os.getcwd()):
     r = random.randint(1, 11)
-    if r > 2:
+    if r > int(sys.argv[1]):
         os.remove(f)
 print("completed")
 
